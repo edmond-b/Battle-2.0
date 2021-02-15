1683086b63f0a786e64590998d867c981e827856
Edmond <edmond.b@hotmail.co.uk>
2021-02-15 18:21:37 +0000

    Passing second test - can also view player 2s HP

--- spec/features/names_spec.rb
+++ spec/features/names_spec.rb
@@ -7,3 +7,13 @@ feature "Testing Names" do
     expect(page).to have_content "Player 1 Vs Player 2"
   end
 end
+
+feature "Testing HP" do
+  scenario "Can see Player 2s HP" do
+    visit('/')
+    fill_in('player_1', with: "Player 1")
+    fill_in('player_2', with: "Player 2")
+    click_button("Submit")
+    expect(page).to have_content "Player 2: 100HP"
+  end
+end
