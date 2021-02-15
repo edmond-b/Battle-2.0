db16e7bd57b15fade825894b8e06c36863c2ae43
Edmond <edmond.b@hotmail.co.uk>
2021-02-15 17:31:59 +0000

    First test passed can enter and view player names

--- spec/features/testing_infrastructure_spec.rb
+++ spec/features/testing_infrastructure_spec.rb
@@ -4,3 +4,13 @@ feature "Testing infrastructure" do
     expect(page).to have_content 'BATTLE!'
   end
 end
+
+feature "Testing Names" do
+  scenario "Can enter names and they apear on screen" do
+    visit('/form')
+    fill_in('player_1', with: "Player 1")
+    fill_in('player_2', with: "Player 2")
+    click_button("Submit")
+    expect(page).to have_content "Player 1 VS Player 2"
+  end
+end
