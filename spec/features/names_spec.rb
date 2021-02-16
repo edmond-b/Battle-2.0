b2143dffe3d396cc56986b795c228fee62f2f413
Edmond <edmond.b@hotmail.co.uk>
2021-02-16 11:49:19 +0000

    Refactored tests into helper methods to keep code dry

--- spec/features/names_spec.rb
+++ spec/features/names_spec.rb
@@ -1,19 +1,13 @@
 feature "Testing Names" do
   scenario "Can enter names and they apear on screen" do
-    visit('/')
-    fill_in('player_1', with: "Player 1")
-    fill_in('player_2', with: "Player 2")
-    click_button("Submit")
+    sign_in_and_play
     expect(page).to have_content "Player 1 Vs Player 2"
   end
 end
 
 feature "Testing HP" do
   scenario "Can see Player 2s HP" do
-    visit('/')
-    fill_in('player_1', with: "Player 1")
-    fill_in('player_2', with: "Player 2")
-    click_button("Submit")
+    sign_in_and_play
     expect(page).to have_content "Player 2: 100HP"
   end
 end
